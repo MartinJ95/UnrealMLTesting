1ad1b58d1d54770afc4b32f8e9814422bd88c265
MartinJ95 <martinjones95@gmx.co.uk>
2025-03-21 13:30:58 +0000

    addition of gameplay tags

--- MLTrainTesting/Source/MLTrainTesting/MLTrainTesting.Build.cs
+++ MLTrainTesting/Source/MLTrainTesting/MLTrainTesting.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using UnrealBuildTool.Rules;
 
 public class MLTrainTesting : ModuleRules
 {
@@ -8,7 +9,7 @@ public class MLTrainTesting : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
