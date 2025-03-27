72d5b52754865cfaa12fe73eb6827026bac46abe
MartinJ95 <martinjones95@gmx.co.uk>
2025-03-27 19:56:08 +0000

    checkpoint and begin overlap addition

--- MLTrainTesting/Source/MLTrainTesting/Checkpoint.cpp
+++ MLTrainTesting/Source/MLTrainTesting/Checkpoint.cpp
@@ -1,4 +1,7 @@
 #include "Checkpoint.h"
+#include "Enemy.h"
+#include "Kismet/GameplayStatics.h"
+#include "Game.h"
 #include "Components/SphereComponent.h"
 
 UE_DEFINE_GAMEPLAY_TAG_COMMENT(Checkpoint, "Checkpoint", "Checkpoint Tag");
@@ -13,4 +16,19 @@ ACheckPoint::ACheckPoint()
 	SphereCol->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	SphereCol->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
 	SphereCol->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+}
+
+void ACheckPoint::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	AEnemy* e = Cast<AEnemy>(OtherActor);
+
+	if (!e)
+		return;
+
+	ATowerDefense* gm = Cast<ATowerDefense>(UGameplayStatics::GetGameMode(GetWorld()));
+	
+	if (!gm)
+		return;
+
+	gm->EnemyReachCheckpoint(e);
 }
--- MLTrainTesting/Source/MLTrainTesting/Checkpoint.h
+++ MLTrainTesting/Source/MLTrainTesting/Checkpoint.h
@@ -14,6 +14,7 @@ class ACheckPoint : public AActor
 	GENERATED_BODY()
 public:
 	ACheckPoint();
+	void NotifyActorBeginOverlap(AActor* OtherActor) override final;
 public:
 	UPROPERTY(EditAnywhere)
 	USceneComponent* Root;
