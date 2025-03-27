7bfc618e73a0c064d8d05846d983b4fd8adfa095
MartinJ95 <martinjones95@gmx.co.uk>
2025-03-27 19:57:01 +0000

    project include plug ins

--- MLTrainTesting/Source/MLTrainTesting/MLTrainTesting.Build.cs
+++ MLTrainTesting/Source/MLTrainTesting/MLTrainTesting.Build.cs
@@ -9,7 +9,7 @@ public class MLTrainTesting : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
